5f5dcc795a05c4442afd01225b330c0f6746fcbc
wyr0o <brykin-2006@mail.ru>
2025-03-29 13:26:19 +0300

    Add files via upload

---
+++ вебинар 7 задание 2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a = 10;
+	int b = 5;
+
+	cout << "a = " << a << ", b = " << b <<endl;
+
+	int temp = a;
+	a = b;
+	b = temp;
+	
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+}
