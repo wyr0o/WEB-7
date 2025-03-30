00279715200ba8e899f55ef7a64cf2ea85ad8c95
wyr0o <brykin-2006@mail.ru>
2025-03-30 18:49:16 +0300

    Add files via upload

---
+++ web 7 задание 2 сложное.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int swapp(int& a, int& b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	return 0;
+}
+
+int main()
+{
+	int a = 131412;
+	int b = 4434213;
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+	swap(a, b);
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+}
