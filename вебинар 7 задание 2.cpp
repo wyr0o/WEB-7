60673d5acb962f40a0eed31f04ee9ccb49833d94
wyr0o <brykin-2006@mail.ru>
2025-03-30 18:46:50 +0300

    Add files via upload

---
+++ вебинар 7 задание 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int swapp(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+	return 0;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 5;
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+	swapp(a, b);
+
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+}
