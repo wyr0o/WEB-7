65697821d60d562a8971547211570fe982d2d78d
wyr0o <brykin-2006@mail.ru>
2025-03-30 18:42:50 +0300

    Add files via upload

---
+++ web 7 задание 2 сложное.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a = 131412;
+	int b = 4434213;
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+
+	cout << "a = " << a << ", b = " << b << endl;
+
+}
